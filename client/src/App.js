e640b5b9d6810bc61261c25a3cc37d208d608600
Radoslav Pavlinchov <radoslav.pavlinchov@gmail.com>
2020-07-13 00:01:31 +0300

    feat: created navigation and footer components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import Navigation from './components/navigation';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
+
+      <Navigation />
+      <Footer />
+
     </div>
   );
 }
