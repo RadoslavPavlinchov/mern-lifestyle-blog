730ac9cb69fa6b0083650c2e1a312e9f5f0423b9
Radoslav Pavlinchov <radoslav.pavlinchov@gmail.com>
2020-07-24 14:02:19 +0300

    feat: register page - added validation, css module, eventHandlers

--- client/src/components/input/index.js
+++ client/src/components/input/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Input = ({ label, id, value, onChange }) => {
-    return(
-        <div>
-            <label htmlFor={id}>{label}</label>
-            <input id={id} value={value} onChange={onChange} />
-        </div>
+const Input = ({ label, error, id, value, onChange, onBlur, type }) => {
+    return (
+        <label>
+            <span htmlFor={id}>{label}</span>
+            <input type={type} id={id} value={value} onChange={onChange} onBlur={onBlur} />
+            {/* {error ? (<span>This is not a valid email!</span>) : null} */}
+        </label>
     )
 }
 
