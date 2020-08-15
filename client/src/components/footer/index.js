f74425096c55a56efa9503828937ce4873e5655f
Radoslav Pavlinchov <radoslav.pavlinchov@gmail.com>
2020-08-15 09:16:16 +0300

    fix: changed the path for the footer

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './index.module.css';
-import Newsletter from '../article/article-sidebar-newsletter';
+import Newsletter from '../article/article-sidebar/article-sidebar-newsletter';
 
 const Footer = () => {
     return (
